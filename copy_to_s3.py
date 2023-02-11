d9b7c1d2f7e9a1d0fff2afeac7e6f68b961e6068
Evan Toyberg <toyberge@moravian.edu>
2023-02-11 15:14:50 -0500

    Add file that saves data to an S3 bucket
    Saves data stored in user's ~/data/data folder into an S3 bucket. Bucket
    name and AWS profile name are in the file, the profile must be
    preconfigured in ~/.aws for the script to run.

---
+++ copy_to_s3.py
@@ -0,0 +1,39 @@
+import boto3
+import os
+
+
+def upload_to_s3(s3, local_file, s3_key):
+    '''
+    upload the file to s3 bucket
+    parameters:
+        local_file: the absolute path of the file to upload
+        s3_key: the relative path of the file within the [local_file] directory
+    '''
+    bucket_name = "mirrulations-data"
+
+    s3.upload_file(local_file, bucket_name, s3_key)
+
+
+def main():
+    '''
+    get each file in the data directory and its relative path within the directory
+    uses the relative path as the file's s3 key.
+    '''
+    session = boto3.Session(profile_name="mirrulations_client")
+    s3 = session.client("s3")
+
+    data_dir = os.path.expanduser("~/data/data") # get the absolute path of the data directory
+    bytes_uploaded = 0
+    files_uploaded = 0
+    for root, dirs, files in os.walk(data_dir):
+        for filename in files:
+            local_path = os.path.join(root, filename) # get the full absolute path of the file
+            s3_path = os.path.relpath(local_path, data_dir) # get the relative path of the file from within the data directory 
+            upload_to_s3(s3, local_path, s3_path)
+            bytes_uploaded += os.path.getsize(local_path)
+            files_uploaded += 1
+            if files_uploaded % 100 == 0:
+                print(f"{bytes_uploaded} bytes uploaded")
+
+if __name__ == "__main__":
+    main()
